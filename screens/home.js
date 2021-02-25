9a871203d8a722af7999f8e370dbc6cb98567daa
Joshua-Simon <joshy.d.simon@gmail.com>
2021-02-25 15:06:51 +1300

    initial navigation stack transition working

--- screens/home.js
+++ screens/home.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import {  View, Text, Button } from 'react-native';
 import { globalStyles } from './styles/globalStyles'
 
-export default function Home() {
+export default function Home(props) {
+
+  const { navigation } = props
+
+  const pressHandler = () => {
+    navigation.navigate('ReviewDetails')
+  }
+
   return (
     <View style={globalStyles.container}>
       <Text style = {globalStyles.title}>Home Screen</Text>
+      <Button title = 'go to review deets' onPress = {pressHandler}/>
     </View>
-  );
+  )
 }
 
