71ffd77752f12860cb9317e756d9b91c134eaa38
Joshua-Simon <joshy.d.simon@gmail.com>
2021-03-09 15:34:16 +1300

    modal added

--- screens/home.js
+++ screens/home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import {  View, Text, FlatList, TouchableOpacity } from 'react-native';
+import {  StyleSheet, View, Text, FlatList, TouchableOpacity, Modal } from 'react-native';
 import { globalStyles } from './styles/globalStyles'
+import { MaterialIcons } from '@expo/vector-icons'
 import Card from '../shared/card'
 
 export default function Home(props) {
@@ -9,13 +10,30 @@ export default function Home(props) {
     { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
     { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
     { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
-  ]);
-
+  ])
+  const [modalOpen,setModalOpen] = useState(false)
 
   const { navigation } = props
 
   return (
     <View style={globalStyles.container}>
+      <Modal visible = {modalOpen} animationType = 'slide'>
+        <View style = {styles.modalContent}>
+          <MaterialIcons
+            name = 'close'
+            size = {24}
+            onPress = {() => setModalOpen(false)}
+            style = {{...styles.modalToggle, ...styles.modalClose}}
+          />
+          <Text>Hello from the modal :)</Text>
+        </View>
+      </Modal>
+      <MaterialIcons
+      name = 'add'
+      size = {24}
+      onPress = {() => setModalOpen(true)}
+      style = {styles.modalToggle}
+      />
       <FlatList
       data = {reviews}
       renderItem = {({item}) => (
@@ -29,4 +47,22 @@ export default function Home(props) {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  modalContent:{
+    flex: 1
+  },
+  modalToggle:{
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: 'center'
+  },
+  modalClose:{
+    marginTop: 20,
+    marginBottom:0
+  }
+})
 
